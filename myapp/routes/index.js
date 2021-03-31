58863fea1c8d3393726e5b0edb7810362a6245a4
dany gbelidji <dany.gbelidji@live.fr>
2021-03-31 10:47:59 +0200

    ✨ADD : indes.js > gestion des données de film via JSON

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+let fs = require('fs');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'BestMovies' });
+
+  fs.readFile('./data/movies.json', (err, data) => {
+    if (err) throw err
+    // On stocke les données du fichier JSON dans un variable
+    let dataMovies = JSON.parse(data)
+    // console.log(dataMovies)
+
+    // On peut ainsi envoyer le contenue du JSON dans notre fichier .pug
+    res.render('index', {
+      title: "BestMovies",
+      dataMovies
+    })
+  })
 });
 
 module.exports = router;
