6d0d8e06ecf99fd7bd9ea176719448ef3c588f74
dany gbelidji <dany.gbelidji@live.fr>
2021-03-31 14:17:14 +0200

    ✨ADD : création d'une page movie en fonction du film choisi

--- myapp/app.js
+++ myapp/app.js
@@ -7,6 +7,7 @@ var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var movieRouter = require('./routes/movie');
 
 var app = express();
 
@@ -28,14 +29,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/movie', movieRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ myapp/routes/movie.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+let fs = require('fs');
+
+/* GET movie page. */
+
+//Définir la route en fonction d'un id => film1, film2 ... 
+
+router.get('/:id', function (req, res, next) {
+    console.log(req.params.id)
+    let idMovie = req.params.id
+
+
+    fs.readFile('./data/movies.json', (err, data) => {
+        if (err) throw err
+        let dataMovies = JSON.parse(data)
+
+
+        let filmAAfficher = dataMovies[idMovie]
+
+        console.log(filmAAfficher)
+
+        res.render('movie', {
+            title: "BestMovies",
+            filmAAfficher: filmAAfficher
+        })
+    })
+
+
+})
+
+
+module.exports = router;
